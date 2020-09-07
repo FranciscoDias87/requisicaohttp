bfb3c58a43e76376e98323c9bcbd525fde8f3742
FranciscoDias87 <franciscocpddias@hotmail.com>
2020-09-07 11:03:42 -0300

    criando post com React e API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nutri: []
+    };
+  }
+
+  componentDidMount() {
+    let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+    fetch(url) //recebe dados do link da api
+      .then((res) => res.json()) //se recebeu certo, converte para json
+      .then((json) => { //se teve sucesso na conversao, popula a state
+        let state = this.state;
+        state.nutri = json;
+        this.setState(state);
+        console.log(json);
+      })
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <header>
+          <strong >React Nutri</strong>
+        </header>
+
+        {this.state.nutri.map((item) => { //mapeia o arrei nutri
+          return (
+            <article className="post" key={item.id} >
+              <strong className="titulo">{item.titulo}</strong>
+              <img className="capa" src={item.capa} alt={item.titulo} />
+              <p className="subtitulo">{item.subtitulo}</p>
+              <a className="botao" href="#">Acessar </a>
+            </article>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
